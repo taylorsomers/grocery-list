c81e6af3830784695b890be5e5eaa8642cdccf9b
Taylor Somers <tsomers@yandex.com>
2020-06-09 13:50:56 -0400

    Add business logic to modify items in the grocery list array such that the character at index position 0 is sliced and replaced by its upper case

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 // Business Logic:
 
-
+function transformGroceryList(arrayParameter) {
+  let newArray = [];
+  arrayParameter.forEach(function(groceryItem) {
+    groceryItem = groceryItem.charAt(0).toUpperCase() + groceryItem.slice(1);
+    newArray.push(groceryItem);
+  });
+  return newArray.sort();
+}
 
 // End Business Logic
 
